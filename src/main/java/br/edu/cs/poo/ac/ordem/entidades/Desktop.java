363f17b22a6f3d5d0e37f1be8e173ac7dd07f410
juliavfe <jvf@cesar.school>
2025-10-11 13:23:41 -0300

    teste

--- src/main/java/br/edu/cs/poo/ac/ordem/entidades/Desktop.java
+++ src/main/java/br/edu/cs/poo/ac/ordem/entidades/Desktop.java
@@ -1,4 +1,18 @@
 package br.edu.cs.poo.ac.ordem.entidades;
 
-public class Desktop {
+import lombok.Getter;
+import lombok.Setter;
+
+public class Desktop extends Equipamento {
+    @Getter @Setter
+    private boolean ehServidor;
+
+    public Desktop(String serial, String descricao, boolean ehNovo, double valorEstimado, boolean ehServidor) {
+        super(serial,descricao,ehNovo,valorEstimado);
+        this.ehServidor = ehServidor;
+    }
+
+    public String getIdTipo() {
+        return "DE";
+    }
 }
